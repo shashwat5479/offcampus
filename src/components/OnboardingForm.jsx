d18ef09d2c89e25d6eb4404c81c5190441ca997a
shashwat5479 <shashwat9252@gmail.com>
2026-08-10 03:31:29 +0530

    Add Confessions to sidebar

--- src/components/OnboardingForm.jsx
+++ src/components/OnboardingForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import CollegePicker from "@/components/Collegepicker";
+import CollegePicker from "@/components/CollegePicker";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
